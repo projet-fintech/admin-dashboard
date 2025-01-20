4abc35f651e5b4fc301ee2d75a51e1bb27036a5b
mouadrguibi <146367397+mouad4949@users.noreply.github.com>
2025-01-20 03:40:38 +0100

    Update api.jsx

--- src/api.jsx
+++ src/api.jsx
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.API_URL; // API FOR USERS (CLIENTS/EMPLOYEES) CRUDS
-const API_URL3 = import.meta.env.API_URL3; // API FOR LOAN MANAGEMENT 
-const API_URL2 = import.meta.env.API_URL2 // API FOR CLIENTS ACCOUNTS MANAGEMENT
-const API_URL4 = import.meta.env.API_URL4// API FOR TRANSACTIONS MANAGEMENT (DEPOSIT/SEND/TRANSACTIONS)
+const API_URL = import.meta.env.VITE_AUTH_API_URL; // API FOR USERS (CLIENTS/EMPLOYEES) CRUDS
+const API_URL3 = import.meta.env.VITE_AUTH_API_URL3; // API FOR LOAN MANAGEMENT 
+const API_URL2 = import.meta.env.VITE_AUTH_API_URL2 // API FOR CLIENTS ACCOUNTS MANAGEMENT
+const API_URL4 = import.meta.env.VITE_AUTH_API_URL4// API FOR TRANSACTIONS MANAGEMENT (DEPOSIT/SEND/TRANSACTIONS)
 
 // Client CRUD functions
 export const createClient = async (clientData) => {
